203002a432554a1ad40b2ca4b1d4976ac64eeb33
jovillacorta <jvill055@ucr.edu>
2018-05-10 18:14:43 -0700

    Working profile page from AllUsers,button needs work

--- app/src/main/java/com/example/james/rchat/ProfileActivity.java
+++ app/src/main/java/com/example/james/rchat/ProfileActivity.java
@@ -2,12 +2,23 @@ package com.example.james.rchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView mDisplayID;
+    private ImageView mProfileImage;
+    private TextView mProfileName, mProfileStatus;//, mProfileFriendsCount;
+    private Button mProfileSendMsgBtn;
 
+    private DatabaseReference mUsersDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +27,34 @@ public class ProfileActivity extends AppCompatActivity {
 
         String user_id = getIntent().getStringExtra("user_id");
 
-        mDisplayID = (TextView) findViewById(R.id.profile_displayName);
-        mDisplayID.setText(user_id);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+        mProfileImage = (ImageView) findViewById(R.id.profile_image);
+        mProfileName = (TextView) findViewById(R.id.profile_displayName);
+        //mProfileFriendsCount = (TextView) findViewById(R.id.profile_totalFriends);
+        mProfileSendMsgBtn = (Button) findViewById(R.id.profile_send_msg_btn);
+        mProfileStatus = (TextView) findViewById(R.id.profile_status);
+
+
+        mUsersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String display_name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mProfileName.setText(display_name);
+                mProfileStatus.setText(status);
+                
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
