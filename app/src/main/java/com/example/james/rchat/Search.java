473e27fb2570787457575d6b691614064e40d6ab
Cy Bae <cybae0804@gmail.com>
2018-05-14 16:09:08 -0700

    Search Function + UI

--- app/src/main/java/com/example/james/rchat/Search.java
+++ app/src/main/java/com/example/james/rchat/Search.java
@@ -1,5 +1,7 @@
 package com.example.james.rchat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -20,8 +22,11 @@ import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Search extends AppCompatActivity {
 
@@ -35,7 +40,7 @@ public class Search extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        userDatabase = FirebaseDatabase.getInstance().getReference("Users");
+        userDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
         searchUser = (EditText) findViewById(R.id.User_Name);
         userButton = (Button) findViewById(R.id.user_search_button);
 
@@ -55,48 +60,57 @@ public class Search extends AppCompatActivity {
     private void firebaseUserSearch(String searchText) {
         Query SearchQuery = userDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
 
-        FirebaseRecyclerOptions<UsersSearch> options =
-                new FirebaseRecyclerOptions.Builder<UsersSearch>()
-                        .setQuery(SearchQuery, UsersSearch.class)
+        FirebaseRecyclerOptions<Users> options =
+                new FirebaseRecyclerOptions.Builder<Users>()
+                        .setQuery(SearchQuery, Users.class)
                         .build();
 
-        FirebaseRecyclerAdapter<UsersSearch, UsersSearchViewHolder> adapterTwo = new FirebaseRecyclerAdapter<UsersSearch, UsersSearchViewHolder>(options) {
+        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<Users, UsersActivity.UserViewHolder>(options) {
             @Override
-            protected void onBindViewHolder(UsersSearchViewHolder holder, int position, UsersSearch model) {
-                holder.setDetails(model.getName(), model.getStatus());
-            }
-
-            @Override
-            public UsersSearchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            public UsersActivity.UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
                 View view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.list_layout, parent, false);
-                return new UsersSearchViewHolder(view);
+                        .inflate(R.layout.users_single_layout, parent, false);
+
+                return new UsersActivity.UserViewHolder(view);
             }
+
+            @Override
+            protected void onBindViewHolder(UsersActivity.UserViewHolder userViewHolder, int position, Users model) {
+                userViewHolder.setName(model.name);
+                userViewHolder.setUserImage(model.image, getApplicationContext());
+                final String user_id = getRef(position).getKey();
+                userViewHolder.mView.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+
+                        Intent profileIntent = new Intent(Search.this, ProfileActivity.class);
+                        profileIntent.putExtra("user_id", user_id);
+                        startActivity(profileIntent);
+                    }
+                });
+            }
+
         };
-
-        resultList.setAdapter(adapterTwo);
-
+        resultList.setAdapter(adapter);
+        adapter.startListening();
     }
 
-    public static class UsersSearchViewHolder extends RecyclerView.ViewHolder{
-
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
         View mView;
-
-        public UsersSearchViewHolder (View itemView)
-        {
+        public UserViewHolder(View itemView) {
             super(itemView);
-
             mView = itemView;
         }
 
-        public void setDetails(String user_Name, String userStatus){
-            TextView user_name = (TextView) mView.findViewById(R.id.layout_user);
-            TextView user_status = (TextView) mView.findViewById((R.id.layout_status));
+        public void setName(String name){
+            TextView userNameView = (TextView) mView.findViewById(R.id.user_single_name);
+            userNameView.setText(name);
+        }
 
-            user_name.setText(user_Name);
-            user_status.setText(userStatus);
+        public void setUserImage(String imageurl, Context ctx){
+            CircleImageView userImageView = (CircleImageView) mView.findViewById(R.id.user_single_image);
+            Picasso.get().load(imageurl).placeholder(R.drawable.default_pic).into(userImageView);
 
         }
     }
-
 }
