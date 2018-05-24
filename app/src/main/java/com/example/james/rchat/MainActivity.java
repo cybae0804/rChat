ae7650b95a7c8e82b206cde9053e2059b1889195
Emma Rivera <erive011@ucr.edu>
2018-05-24 11:57:45 -0700

    Updated splash screen to send to correct activity depending on if user is logged in or not

--- app/src/main/java/com/example/james/rchat/MainActivity.java
+++ app/src/main/java/com/example/james/rchat/MainActivity.java
@@ -55,21 +55,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if(currentUser == null){
-
-            sendToStart();
-
-        }else{
-            mUserRef.child("online").setValue("true");
-        }
-    }
-
-    @Override
     protected void onStop() {
         super.onStop();
 
