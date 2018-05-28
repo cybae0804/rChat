9c473c71f2eac1746100f7bd3f1683acc9d52726
SamuelLabrador <Sam.Labrador@Yahoo.com>
2018-05-27 20:03:56 -0700

    fixed bug with boot

--- app/src/main/java/com/example/james/rchat/MainActivity.java
+++ app/src/main/java/com/example/james/rchat/MainActivity.java
@@ -11,9 +11,6 @@ import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ServerValue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    private DatabaseReference mUserRef;
 
     private TabLayout mTabLayout;
 
@@ -38,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolBar);
         getSupportActionBar().setTitle("Rchat");
 
-        mUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
-
 
         //Tabs
         mViewPager = (ViewPager) findViewById(R.id.main_tabPager);
@@ -49,8 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         mTabLayout = (TabLayout) findViewById(R.id.main_tabs);
         mTabLayout.setupWithViewPager(mViewPager);
-        TabLayout.Tab tab = mTabLayout.getTabAt(1);
-        tab.select();
+
 
     }
 
@@ -64,21 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
             sendToStart();
 
-        }else{
-            mUserRef.child("online").setValue("true");
         }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if(currentUser != null){
-            mUserRef.child("online").setValue(ServerValue.TIMESTAMP);
-        }
-
     }
 
     private void sendToStart(){
@@ -94,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+
         return true;
     }
 
@@ -102,15 +83,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         super.onOptionsItemSelected(item);
 
-        if(item.getItemId() == R.id.main_create_group_chat){
-
-            Intent startGroupChat = new Intent(MainActivity.this, GroupCreationActivity.class );
-            startActivity(startGroupChat);
-        }
-
         if(item.getItemId() == R.id.main_logout_btn){
-
-            mUserRef.child("online").setValue(ServerValue.TIMESTAMP);
 
             FirebaseAuth.getInstance().signOut();
             sendToStart();
